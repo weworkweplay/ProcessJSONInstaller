5a9117c83e2adb5f59821467265e0d247fea1930
owzim <nope@nope.nope>
2014-05-25 08:01:34 +0200

    Fixes

--- classes/Module.php
+++ classes/Module.php
@@ -142,7 +142,7 @@ class Module {
             if (!$t) {
                 $t = new Template();
                 $t->name = $templateJSON->name;
-                if ($templateJSON->label) {
+                if (isset($templateJSON->label)) {
                     $t->label = $templateJSON->label;
                 }
             }
