a44ae84225b0b819a9f7502fddf2cc15b7ea228a
Pieter Beulque <pieterbeulque@gmail.com>
2014-05-27 15:00:45 +0200

    JSONLoader polishing

--- classes/JSONLoader.php
+++ classes/JSONLoader.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace JSONInstaller;
-/**
- * Singleton
- */
+
 class JSONLoader {
 
+    /**
+     * Singleton
+     */
     private static $instance;
 
     public $dir;
@@ -16,14 +17,10 @@ class JSONLoader {
     }
 
     public static function create() {
-        if (self::$instance) {
-            return self::$instance;
-        }
-        return self::$instance = new self();
+        return (self::$instance) ? self::$instance : self::$instance = new self();
     }
 
     public function getModules() {
-
         if ($this->modules) {
             return $this->modules;
         }
@@ -42,46 +39,44 @@ class JSONLoader {
 
     public static function loadModule($file, $dir = null) {
         $instance = self::create();
-        if (!$dir) {
-            $dir = $instance->dir;
-        }
+        $dir = is_null($dir) ? $instance->dir : $dir;
         $fullPath = $dir . DIRECTORY_SEPARATOR . $file;
-        
+
         if (!file_exists($fullPath)) {
             return false;
         }
-        
+
         $slug = self::filenameToSlug($file);
-        
+
         // return if module has already been loaded
         if(isset($instance->modules[$slug])) {
             return $instance->modules[$slug];
         }
-        
+
         $json = json_decode(file_get_contents($fullPath));
-        
+
         // this line is necessary to add the module to the list of all modules
         // prevention of curcular reference, see JSONLoader::isModuleLoaded()
         $instance->modules[$slug] = $slug;
-        
+
         $module = Module::createFromJSON($json);
         $instance->modules[$slug] = $module;
         $module->slug = $slug;
 
         return $module;
     }
-    
+
     public function getModule($slug) {
         $modules = $this->getModules();
-        if(isset($modules[$slug])) {
-            return $modules[$slug];
-        } else {
-            return false;
-        }
-    }
-    
-    public static function filenameToSlug($filename) {
-        return substr($filename, 0, -5);
+        return !empty($modules[$slug]) ? $modules[$slug] : false;
     }
 
+    public static function filenameToSlug($filename) {
+        if (strpos($filename, '.json') === -1) {
+            throw new Exception('This is not a valid module');
+            die;
+        }
+
+        return wire('sanitizer')->pageName(str_replace('.json', '', $filename));
+    }
 }
